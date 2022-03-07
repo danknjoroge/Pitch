fa8364c39b08701e0e051dd641576d10dea0a9e5
danknjoroge <dank8505@gmail.com>
2022-03-07 07:21:40 +0300

    added a navbar

---
+++ tests/test_user.py
@@ -0,0 +1,36 @@
+import unittest
+from app.models import User
+
+class TestUser(unittest.TestCase):
+
+    # def setUp(self):
+    #     self.new_user = User(password="octopizzo")
+
+    # def test_password_setter(self):
+    #     self.assertTrue(self.new_user.pass_secure is not None)
+
+
+    def setUp(self):
+        """
+         method that creates an instance of our User class
+        """
+        self.new_user = User(password = "octopizzo")
+
+    def test_password_setter(self):
+        """
+         ascertains that when password is being hashed and the pass_secure contains a value
+        """
+        self.assertTrue(self.new_user.password_hash is not None)
+    
+    def test_no_access_password(self):
+        """
+         confirms that our application raises an AttributeError when we try and access the password property
+        """
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        """
+        confirms that our password_hash can be verified when we pass in the correct password.
+        """
+        self.assertTrue(self.new_user.verify_password('octopizzo'))
